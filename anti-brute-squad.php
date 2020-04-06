3fdb819b4e00c04f09ee17b8f220378d9b0a1448
David Dashifen Kees <dashifen@dashifen.com>
2020-04-06 09:09:47 -0400

    updating plugin header

--- anti-brute-squad.php
+++ anti-brute-squad.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * Plugin Name: ACF Sharing Settings
- * Description: An ACF field group for a settings page relating to how a site appears on Twitter and Facebook.
+ * Plugin Name: Anti Brute Squad
+ * Description: An WordPress must-use plugin that limits login attempts to mitigate brute force attacks.
  * Author URI: mailto:dashifen@dashifen.com
  * Author: David Dashifen Kees
- * Version: 1.2.0
+ * Version: 1.0.2
  *
  * @noinspection PhpStatementHasEmptyBodyInspection
  * @noinspection PhpIncludeInspection
