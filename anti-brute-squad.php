65273ad6410da7594828ae70461e87bed5564c59
David Dashifen Kees <dashifen@dashifen.com>
2020-04-06 14:52:25 -0400

    wp-handler update; better autoloader inclusion

--- anti-brute-squad.php
+++ anti-brute-squad.php
@@ -14,20 +14,12 @@
 use Dashifen\AntiBruteSquad\AntiBruteSquad;
 use Dashifen\WPHandler\Handlers\HandlerException;
 
-// the following snippet finds the appropriate autoloader starting from a
-// where Dash likes to put it and ending at a reasonable (but unlikely)
-// default.  note that the $autoloader variable is set in the if-conditionals,
-// so they don't need statement bodies; all their work is done during each
-// test.  then, we try requiring what we end up with.
-
-if (file_exists($autoloader = dirname(ABSPATH) . '/deps/vendor/autoload.php'));
-elseif ($autoloader = file_exists(dirname(ABSPATH) . '/vendor/autoload.php'));
-elseif ($autoloader = file_exists(ABSPATH . 'vendor/autoload.php'));
-else $autoloader = 'vendor/autoload.php';
-require_once $autoloader;
-
-try {
-    (new AntiBruteSquad())->initialize();
-} catch (HandlerException $exception) {
-    wp_die('Unable to initialize Anti Brute Squad.');
-}
+(function () {
+    try {
+        AntiBruteSquad::requireAutoloader();
+        $antiBruteSquad = new AntiBruteSquad();
+        $antiBruteSquad->initialize();
+    } catch (HandlerException $e) {
+        wp_die($e->getMessage();
+    }
+})();
