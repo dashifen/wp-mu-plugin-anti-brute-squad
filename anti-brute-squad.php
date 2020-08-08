6e6f6df1be235ea3776e2776a2b22c4a54b16f04
David Dashifen Kees <dashifen@dashifen.com>
2020-08-08 10:02:39 -0400

    fixing autoloader (else)if-blocks

--- anti-brute-squad.php
+++ anti-brute-squad.php
@@ -16,8 +16,8 @@ use Dashifen\AntiBruteSquad\AntiBruteSquad;
 use Dashifen\WPHandler\Handlers\HandlerException;
 
 if (file_exists($autoloader = dirname(ABSPATH) . '/deps/vendor/autoload.php'));
-elseif ($autoloader = file_exists(dirname(ABSPATH) . '/vendor/autoload.php'));
-elseif ($autoloader = file_exists(ABSPATH . 'vendor/autoload.php'));
+elseif (file_exists($autoloader = dirname(ABSPATH) . '/vendor/autoload.php'));
+elseif (file_exists($autoloader = ABSPATH . 'vendor/autoload.php'));
 else $autoloader = 'vendor/autoload.php';
 require_once $autoloader;
 
