dd8ab27e266b6002c10e0360329678817248ba88
David Dashifen Kees <dashifen@dashifen.com>
2021-10-08 05:52:58 -0400

    tagging for version 3

--- anti-brute-squad.php
+++ anti-brute-squad.php
@@ -6,7 +6,7 @@
  * Author URI: mailto:dashifen@dashifen.com
  * Author: David Dashifen Kees
  * Text Domain: anti-brute-squad
- * Version: 2.0.0
+ * Version: 3.0.0
  *
  * @noinspection PhpStatementHasEmptyBodyInspection
  * @noinspection PhpIncludeInspection
